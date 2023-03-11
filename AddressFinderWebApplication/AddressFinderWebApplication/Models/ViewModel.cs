8620e521a209a7c978ef3516422325878fc8b46d
Archana Janardhana Vinoda Kumari <ajanardhanavinodakum@seattleu.edu>
2023-03-10 16:14:05 -0800

    Uploaded the base project

---
+++ AddressFinderWebApplication/AddressFinderWebApplication/Models/ViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AddressFinderWebApplication.Models
+{
+    public class ViewModel
+    {
+        public List<SelectListItem> Options { get; set; }
+    }
+}
---
+++ AddressFinderWebApplication/AddressFinderWebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AddressFinderWebApplication.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AddressFinderWebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        ViewModel viewModel = new ViewModel();
+        public static List<SelectListItem>? Options { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;                      
+        }
+
+
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
